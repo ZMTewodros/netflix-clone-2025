1126298166be63e92c4e6228f354e06ca27ba0e0
ZMTewodros <tewodrosmelkamu272121@gmail.com>
2025-07-13 10:25:25 -0500

    netflixBanner

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Header from "../../components/Header/Header"
 import Footer from "../../components/footer/Footer"
+import Banner from "../../components/banner/Banner"
 
 function Home() {
   return (
     <>
+    
     <Header/>
+    <Banner/>
     <Footer/>
     
     
